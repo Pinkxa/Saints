aaaef11cc2f10c3ee7db6969c6f3aa739b7587dd
Pretty <pretty@projectcodex.co>
2015-12-02 16:49:57 +0200

    modified the app and styled it

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var EduApp = angular.module("EduApp", []);
 EduApp.controller('MainCtrl', ['$scope', function($scope){
+
 	$scope.venues = [
 				{
 					name : "Langa Primary",
@@ -17,4 +18,37 @@ EduApp.controller('MainCtrl', ['$scope', function($scope){
 					longitude : 300
 		}
 	]
-}])
+
+	$scope.name = "Pretty";
+
+  // var mysrclat= 0; var mysrclong = 0;
+  // $scope.latitude = "123";
+  // $scope.latitudeDisplay = function(){
+  // 	return $scope.latitude;
+  // }
+
+  // $scope.longitude = "123";
+  // $scope.longitudeDisplay = function(){
+  // 	return $scope.longitude;
+  // }
+
+
+  $scope.getLocation = function(position){
+  	$scope.latitude = position.coords.latitude; 
+  	$scope.longitude = position.coords.longitude;
+  	    $scope.$apply(); //this triggers a $digest
+
+               
+                // console.log(mysrclat);
+                // console.log(mysrclong);
+  };
+
+  $scope.nearMe = function() {
+  	
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition($scope.getLocation);
+        
+    }
+    //$scope.latitude = "HELLO";
+}
+}]);
